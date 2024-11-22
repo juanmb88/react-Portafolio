a83e06afd9eac15eb67f7d541d87fabf86999ba2
Juan-Balugano <juanbalugano@gmail.com>
2024-11-22 10:46:09 -0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(() => ({
   plugins: [react()],
   server: {
-    port: parseInt(import.meta.env.VITE_PORT) || 3000, // Usamos import.meta.env
+    port: parseInt(import.meta.env.VITE_PORT) || "3000", // Usamos import.meta.env
     strictPort: true,
   },
   build: {
