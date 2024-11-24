120853071ca51938613dfacc9ace0e5b136143c4
Juan-Balugano <juanbalugano@gmail.com>
2024-11-24 12:36:03 -0300

    second commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Permite que Railway escuche desde cualquier IP
+    port:  4173, // Usa el puerto asignado por Railway
+  },
 })
